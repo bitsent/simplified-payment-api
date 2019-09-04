a73bb770e5ec09789b8ebcff3f686e1f0300db5d
Aleksandar Nikolaev Dinkov <and@milestone.dk>
2019-09-04 16:26:18 +0300

    more detailed basic info

--- routes/swaggerDoc.js
+++ routes/swaggerDoc.js
@@ -5,10 +5,13 @@ var options = {
   swaggerDefinition: {
     // Like the one described here: https://swagger.io/specification/#infoObject
     info: {
-      title: 'Simplified Payment API - BitSent',
+      title: "BitSent's Simplified Payment API (beta version)",
       version: '0.1.0',
-      description: 'An API for BIP0270 payments'
-    },
+      description: 'An API for BIP0270 payments.' +
+      '\nGit: https://github.com/bitsent/simplified-payment-api' +
+      '\nThis API is a bridge between the way payments have been done until now and BIP-270' +
+      '\nThis API will be the default BIP270 payment server for BitBtn.'
+    }
   },
   // List of files to be processes. You can also set globs './routes/*.js'
   apis: ['./routes/payment.js']
