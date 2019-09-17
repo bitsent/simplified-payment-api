b5fe8214505591686774d2d469fe6414eac4c012
Aleksandar Nikolaev Dinkov <and@milestone.dk>
2019-09-17 18:00:43 +0300

    less unnecessary validation

--- routes/bsvalias.js
+++ routes/bsvalias.js
@@ -2,24 +2,23 @@ var buildRouter = require('@moneybutton/express-paymail').buildRouter
 var settings = require('../settings.json')
 var bsv = require('@moneybutton/paymail-client/node_modules/bsv')
 
-var pubkey = bsv.PublicKey.fromPrivateKey(bsv.PrivateKey.fromString(settings.privkey))
+var pubkey = bsv.PublicKey.fromPrivateKey(bsv.PrivateKey.fromString(settings.privkey)).toHex()
 
 var callbacks = {
   basePath: '/api/bsvalias',
   getIdentityKey: async (name, domain) => {
-    if (name + '@' + domain === settings.serverPaymail) { return pubkey } else { return undefined }
+    return pubkey
   },
   getPaymentDestination: async (name, domain, body, helpers) => {
     return helpers.p2pkhFromAddress(settings.receiveAddress)
   },
   verifyPublicKeyOwner: async (name, domain, publicKeyToCheck) => {
-    return publicKeyToCheck === pubkey &&
-      name + '@' + domain === settings.serverPaymail
+    return publicKeyToCheck === pubkey
   }
 }
 
-var create = function (BASE_URL) { 
-  return buildRouter(BASE_URL, callbacks);
+var create = function (BASE_URL) {
+  return buildRouter(BASE_URL, callbacks)
 }
 
 module.exports = { create }
